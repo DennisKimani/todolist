bb98db96d98191608002dff88c0de642af222c7e
DennisKimani <dennisngugi.dk@gmail.com>
2017-05-31 07:53:36 +0300

    added a click function

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,21 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h3 (click)="doStuff()" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
   </div>
   `
 })
 
 export class AppComponent {
   public tasks: Task[] = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
+      new Task("Going out for swimming.", 0),
+      new Task("practising how to code.", 1),
+      new Task("being sarcastic on most of the things i do.", 2),
+      new Task("doing what i do best.", 3)
   ];
+  doStuff() {
+    alert("You totally just clicked on a task!");
+  }
 }
 
 export class Task {
