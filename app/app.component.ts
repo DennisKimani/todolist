3cfb5eb41e1dd06355af782afaefd7f9557c6b05
DennisKimani <dennisngugi.dk@gmail.com>
2017-05-31 12:03:40 +0300

    added a new file of pies-list.component.app

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,28 +5,33 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+    <h3>Here are my favorite pies!</h3>
+    <div *ngFor="let currentPie of favoritePies">
+      <p>{{currentPie}}</p>
+    </div>
     <div *ngFor="let currentTask of tasks">
       <h3>{{ currentTask.description }}</h3>
       <button (click)="showDetails(currentTask)">Edit</button>
     </div>
     <div *ngIf="selectedTask">
       <h1>Edit Task</h1>
-    <div>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-    </div>
-    <div>
-      <label>Enter Task ID:</label>
-      <input [(ngModel)]="selectedTask.id">
-      <button (click)="finishedEditing()">Done</button>
+      <div>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+      </div>
+      <div>
+        <label>Enter Task ID:</label>
+        <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
-</div>
   `
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
+  favoritePies: string[] = ["Apple", "Banana Cream", "Blackberry"];
+  tasks: Task[] = [
       new Task("Create To-Do List app.", 0),
       new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
